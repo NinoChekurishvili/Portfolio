280b96e9dea48f87d114bdd0e14f3ac0167236b3
NinoChekurishvili <115742132+NinoChekurishvili@users.noreply.github.com>
2022-12-11 17:25:55 +0400

    subscribe button

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ let burgerbar = document.getElementById("burger-bar");
 burgerbar.addEventListener('click', function(){
     navigation.classList.toggle('navActive')
 })
+
+
+
+function ValidateEmail(input) {
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;  
+    if (input.value.match(validRegex)) {
+      document.form1.text1.focus();
+      return true;
+    } else {  
+      alert("Invalid email address!");  
+      document.form1.text1.focus();  
+      return false;  
+    }  
+  }
